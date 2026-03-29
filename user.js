9c93eb6ae3972097de7fef241aa35c8b5f76e969
Pavel123-serk <skvpavelskvarko@gmail.com>
2026-03-29 12:56:09 +0200

    123

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
-const urlParams = new URLSearchParams (window. location.search);
-const id = urlParams.get ('id');
+const urlParams = new URLSearchParams(window. location.search);
+const id = urlParams.get('id');
 
-let profile = document.getElementById ('profile');
+let profile = document.getElementById('profile');
 let url = 'https://my-json-server.typicode.com/RobocodeSchool/marketplace';
 
 //User details request
