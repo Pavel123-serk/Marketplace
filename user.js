1273926ba4453a926aa368c1e2198e5ebfb6ec93
Pavel123-serk <skvpavelskvarko@gmail.com>
2026-04-19 13:23:18 +0200

    454

--- user.js
+++ user.js
@@ -1,29 +1,29 @@
-const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get('id');
+// const urlParams = new URLSearchParams(window.location.search);
+// const id = urlParams.get('id');
 
-let profile = document.getElementById('profile');
-let produktsGrid = document.getElementById('products-grid');
-let url = 'https://my-json-server.typicode.com/RobocodeSchool/marketplace';
+// let profile = document.getElementById('profile');
+// let produktsGrid = document.getElementById('products-grid');
+// let url = 'https://my-json-server.typicode.com/RobocodeSchool/marketplace';
 
-    //User details request
-    let productsRequest = new XMLHttpRequest();
-    productsRequest.open('GET', `${url}/users/${id}`);
-    productsRequest.responseType = 'json';
-    productsRequest.onload = function() {
-        let products = productsRequest.response;
-        produktsGrid.innerHTML = null;
-        products.forEach(p => {
-            produktsGrid.innerHTML += `
-                <div class="product">
-                    <h2 class="product-name">${p.name}</h2>
-                    <img class='product-photo' src='${p.photo_url}' alt='${p.name}'>
-                    <p class="product-price">Price: </b>${p.price}</p>
-                    <p class="product-description"><b>Description: </b>${p.description}</p>
-                </div>
-            `;
-        });
-    }
-    productsRequest.send();    
+//     //User details request
+//     let productsRequest = new XMLHttpRequest();
+//     productsRequest.open('GET', `${url}/users/${id}`);
+//     productsRequest.responseType = 'json';
+//     productsRequest.onload = function() {
+//         let products = productsRequest.response;
+//         produktsGrid.innerHTML = null;
+//         products.forEach(p => {
+//             produktsGrid.innerHTML += `
+//                 <div class="product">
+//                     <h2 class="product-name">${p.name}</h2>
+//                     <img class='product-photo' src='${p.photo_url}' alt='${p.name}'>
+//                     <p class="product-price">Price: </b>${p.price}</p>
+//                     <p class="product-description"><b>Description: </b>${p.description}</p>
+//                 </div>
+//             `;
+//         });
+//     }
+    // productsRequest.send();    
     //                 <h3>${p.name}</h3>
     //                 <p>${p.description}</p>
     //                 <p>Price: $${p.price.toFixed(2)}</p>
@@ -44,3 +44,52 @@ let url = 'https://my-json-server.typicode.com/RobocodeSchool/marketplace';
     //         <p>Balance: ${user.balance}$</p>
     //     `
     // })
+
+
+    const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+
+let profile = document.getElementById('profile');
+let productsGrid = document.getElementById('user-products-grid');
+
+let url = 'https://my-json-server.typicode.com/jokerok2569/MarketPlace';
+
+//User details request
+let userRequest = new XMLHttpRequest();
+
+userRequest.open('GET',${url}/users/${id});
+userRequest.responseType = 'json'
+userRequest.onload = function() { 
+    let user = userRequest.response;
+    profile.innerHTML = 
+        <h1>${user.name}</h1>
+        <h2>${user.sirname}</h2>
+        <img class="profile-img" src="${user.photo_url}">
+        <p>Balance: ${user.balance}$</p>
+    
+}
+
+userRequest.send();
+
+
+//User products request
+let productsRequest = new XMLHttpRequest();
+
+productsRequest.open('GET',${url}/products?author_id=${id});
+productsRequest.responseType = 'json'
+productsRequest.onload = function() { 
+    let products = productsRequest.response;
+    productsGrid.innerHTML = null;
+    products.forEach(p => {
+        productsGrid.innerHTML += 
+            <div class="product">
+                <h2 class='product-name'>${p.name}</h2>
+                <img class='product-photo' src='${p.photo_url}' alt='${p.name}'>
+                <p class='product-price'><b>Price: </b>${p.price}$</p>
+                <p class='product-description'><b>Description: </b>${p.description}</p>
+            </div>
+        ;
+    });
+}
+
+productsRequest.send();
