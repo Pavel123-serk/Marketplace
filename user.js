e01605c41f798e3d26cccab8d1e7b1ea1717b1fd
Pavel123-serk <skvpavelskvarko@gmail.com>
2026-04-19 13:26:50 +0200

    7878

--- user.js
+++ user.js
@@ -46,7 +46,7 @@
     // })
 
 
-    const urlParams = new URLSearchParams(window.location.search);
+const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get('id');
 
 let profile = document.getElementById('profile');
@@ -61,12 +61,12 @@ userRequest.open('GET',${url}/users/${id});
 userRequest.responseType = 'json'
 userRequest.onload = function() { 
     let user = userRequest.response;
-    profile.innerHTML = 
+    profile.innerHTML = `
         <h1>${user.name}</h1>
         <h2>${user.sirname}</h2>
         <img class="profile-img" src="${user.photo_url}">
         <p>Balance: ${user.balance}$</p>
-    
+    `
 }
 
 userRequest.send();
@@ -81,14 +81,14 @@ productsRequest.onload = function() {
     let products = productsRequest.response;
     productsGrid.innerHTML = null;
     products.forEach(p => {
-        productsGrid.innerHTML += 
+        productsGrid.innerHTML += `
             <div class="product">
                 <h2 class='product-name'>${p.name}</h2>
                 <img class='product-photo' src='${p.photo_url}' alt='${p.name}'>
                 <p class='product-price'><b>Price: </b>${p.price}$</p>
                 <p class='product-description'><b>Description: </b>${p.description}</p>
             </div>
-        ;
+        `;
     });
 }
 
